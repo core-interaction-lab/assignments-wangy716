942d4ca8b43e695c9fa5bdcf81cf23f3fb480950
wangy716 <wangy716@newschool.edu>
2022-04-22 09:43:25 -0400

    assigne-4

--- assignments/assignment-4/main.js
+++ assignments/assignment-4/main.js
@@ -1,11 +1,20 @@
 
 
+const albumContainer = document.getElementById("album-container");
+
     const fetchContent = async () => {
         const response = await fetch('https://interactionlab.space/data/assignment-4-1.json').then(data => data.json());
-    console.log(response);
+    console.log(response.items);
+
+    response.items.forEach(item => {
+        console.log(item)
+        const imgEL = document.createElement("img");
+        imgEL.setAttribute('src', item.images[0].url);
+        albumContainer.append(imgEL);
+        });
     };
 
-    const album = document.getElementById("album");
+
     
 
 
