e0a6000712111afcdc7d0bd2544ad8b6b7edb627
wangy716 <wangy716@newschool.edu>
2022-04-29 09:48:41 -0400

    ex10

---
+++ exercises/exercise-10/js/main.js
@@ -0,0 +1,7 @@
+const addTextarea = document.getElementById("todo-textarea");
+const addTodoBtn = document.getElementById("add-todo-btn");
+const todoContainer = document.getElementById("todo-container");
+
+const state = {  //keeping track the state of app
+    todoItems:[],
+};
