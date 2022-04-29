53915005b3eb891831275ec8d8129735ae29b12c
wangy716 <wangy716@newschool.edu>
2022-04-29 10:26:43 -0400

    ex-10finished

--- exercises/exercise-10/js/main.js
+++ exercises/exercise-10/js/main.js
@@ -5,3 +5,33 @@ const todoContainer = document.getElementById("todo-container");
 const state = {  //keeping track the state of app
     todoItems:[],
 };
+
+const setTodoItems = items => {
+    state.todoItems = items;
+}
+
+const addTodoItem = item => {
+    const todoItemsCopy = state.todoItems.slice();
+    todoItemsCopy.push(item);
+    setTodoItems(todoItemsCopy);
+}
+
+const buildTodoItem = item => {
+    const todoEl = document.createElement('article');
+    todoEl.innerHTML = item;
+    return todoEl;
+};
+
+const main = () => {
+    addTodoBtn.addEventListener('click', evt => {
+        const todoValue = addTextarea.value;
+        if(todoValue.length > 0){
+            addTodoItem(todoValue);
+            todoContainer.innerHTML = '';
+            const todoItemEls = state.todoItems.map(buildTodoItem);
+            todoContainer.append(...todoItemEls);
+        }
+    });
+};
+
+main();
