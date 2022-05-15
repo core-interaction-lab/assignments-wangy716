d8c3ff7f60accfbe7ff7f4ab9391a04fb397de91
wangy716 <wangy716@newschool.edu>
2022-05-15 15:13:59 -0400

    assignment-5

---
+++ assignments/assignment-5/main.js
@@ -0,0 +1,14 @@
+const formEl = document.getElementById('madlib-form');
+
+formEl.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = new FormData(formEl);
+
+    console.log(formData.get('input-1'));
+    const spanEls = document.querySelectorAll('.input-value');
+
+    spanEls.forEach(span => {
+        span.innerHTML = formData.get(span.dataset.input);
+    });
+})
